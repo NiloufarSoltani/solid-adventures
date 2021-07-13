dbe66f2edaedc9e3151e2d33a36463f5bc8136c0
Nill <nilooooyi@gmail.com>
2021-07-13 23:28:11 +0430

    Add my projects

---
+++ color Flipper/app.js
@@ -0,0 +1,41 @@
+let changeColorInterval = null;
+
+function randomFromArray() {
+    let colors = ['red', 'yellow', '#f23', 'green', 'pink', 'tomato', 'brown'];
+    let randomNumber = Math.round(Math.random() * (colors.length - 1));
+    return colors[randomNumber];
+}
+
+function randomColor() {
+    return "#" + Math.random().toString(16).substr(2, 6);
+}
+
+document.getElementById('getFromArrayBtn').addEventListener('click', () => {
+    setBackgroundColor(randomFromArray());
+})
+
+function setBackgroundColor(color) {
+    document.getElementsByTagName('body')[0].style.backgroundColor = color;
+    document.getElementById('currentBg').innerText = color;
+}
+
+// let changeColorTimeout = setTimeout(() => {
+//     setBackgroundColor(randomColor());
+// }, 10000);
+//
+// clearTimeout(changeColorTimeout);
+
+function setColorInterval() {
+    // if (changeColorInterval)
+    //     clearInterval(changeColorInterval);
+
+    changeColorInterval && clearInterval(changeColorInterval);
+
+    changeColorInterval = setInterval(() => {
+        setBackgroundColor(randomColor());
+    }, 50);
+}
+
+function stopColorInterval() {
+    clearInterval(changeColorInterval);
+}
