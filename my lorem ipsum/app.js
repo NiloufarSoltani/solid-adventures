9bfe49ac481a622da42a2af29b1054c38cd7f578
Nill <nilooooyi@gmail.com>
2021-08-13 02:12:05 +0430

    Add my new project: my lorem ipsum

---
+++ my lorem ipsum/app.js
@@ -0,0 +1,48 @@
+let number = document.querySelector('#number');
+let loremApiUrl;
+let waiting = false;
+
+let confirmNumber = document.querySelector('#confirm-number');
+let paragraph = document.querySelector('#paragraphs');
+
+confirmNumber.addEventListener('click', function () {
+    if (waiting) return;
+    waiting = true;
+    loremApiUrl = `http://hipsum.co/api/?type=hipster-centric&paras=${number.value}`;
+    fetch(loremApiUrl)
+        .then(
+            response => response.json()
+        )
+        .then(
+            function (result) {
+                waiting = false;
+                paragraph.innerText = '';
+                paragraph.style.height = "0px";
+
+                // it has a note: create a break line with innerHTML and \n
+                for (let index in result) {
+                    if (+index === (result.length - 1)) {
+                        paragraph.innerHTML += result[index] + `\n`;
+                    } else {
+                        paragraph.innerHTML += result[index] + `\n\n`;
+                    }
+                }
+
+                paragraph.style.height = paragraph.scrollHeight + "px";
+            }
+        )
+        .catch(e => console.log(e));
+})
+
+document.querySelector('#copy').addEventListener('click', function () {
+    paragraph.select();
+    document.execCommand("copy");
+})
+
+window.addEventListener('resize', (e) => {
+    paragraph.style.height = "0px";
+    paragraph.style.height = paragraph.scrollHeight + "px";
+});
+
+
+
