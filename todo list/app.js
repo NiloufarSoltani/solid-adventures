e25283cfe0416218133db035057c461f3c1a4abe
Nill <nilooooyi@gmail.com>
2021-10-29 23:35:25 +0330

    Add notification to actions

--- todo list/app.js
+++ todo list/app.js
@@ -3,7 +3,8 @@ let addBtn = document.querySelector('#add-btn');
 let changeBtn = document.querySelector('#edit-btn');
 let container = document.querySelector('#item-container');
 let clearItems = document.querySelector('#clear-items');
-let itemList = [];
+let notification = document.getElementById('notification');
+let itemList = localStorage.length ? localStorage.getItem('myList').split(',') : [];
 
 const addItem = function (item) {
     if (!item) {
@@ -14,22 +15,30 @@ const addItem = function (item) {
     itemList.push(item);
     initial.value = '';
 
+    showNotification('A new item added!', 'primary');
     render();
 }
 
 const editItem = function (itemIndex, newValue) {
-    itemList[itemIndex] = newValue;
+    if (newValue) {
+        itemList[itemIndex] = newValue;
+    } else {
+        alert('Item name is required!')
+    }
 
-    addBtn.style.display = 'block';
+    addBtn.style.display = 'inline';
     changeBtn.style.display = 'none';
     initial.value = '';
     initial.removeAttribute('data-id');
 
+    showNotification('An item changed!', 'primary');
     render();
 }
 
 const deleteItem = function (itemIndex) {
     itemList.splice(itemIndex, 1);
+
+    showNotification('An item deleted!', 'danger');
     render();
 }
 
@@ -37,6 +46,9 @@ function createItem(initialItem, index) {
     item = document.createElement('li');
     item.className = 'item';
     item.innerText = initialItem;
+
+    let buttonBox = document.createElement('div');
+    buttonBox.className = 'buttonBox';
 
     let deleteBtn = document.createElement('button');
     deleteBtn.className = 'deleteItem';
@@ -56,34 +68,52 @@ function createItem(initialItem, index) {
         initial.focus();
 
         addBtn.style.display = 'none';
-        changeBtn.style.display = 'block';
+        changeBtn.style.display = 'inline';
     });
 
-    item.append(editBtn, deleteBtn);
-
+    buttonBox.append(editBtn, deleteBtn);
+    item.append(buttonBox);
     container.append(item);
 }
 
 function clearAll() {
     itemList.length = 0;
+
+    showNotification('All items deleted!', 'danger');
     render();
+}
+
+function showNotification(message, type) {
+    if (type == 'danger') {
+        notification.style.backgroundColor = '#edc5c5';
+    } else {
+        notification.style.backgroundColor = '#bccfbc';
+    }
+    notification.innerText = message;
+    setTimeout(function () {
+        notification.style.background = 'none';
+        notification.innerText = '';
+        console.log('k')
+    }, 1500);
 }
 
 const render = function () {
     container.innerHTML = '';
-
-    let newItems = itemList.map((item, index) => {
+    localStorage.setItem('myList', itemList);
+    itemList.map((item, index) => {
         createItem(item, index);
     });
 }
+
+render();
 
 clearItems.addEventListener('click', clearAll);
 addBtn.addEventListener('click', () => addItem(initial.value));
 changeBtn.addEventListener('click', () => editItem(initial.dataset.id, initial.value));
 initial.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
         e.preventDefault();
-        if(initial.dataset.id) {
+        if (initial.dataset.id) {
             editItem(initial.dataset.id, initial.value);
         } else {
             addItem(initial.value);
